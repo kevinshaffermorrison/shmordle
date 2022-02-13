b8ebc74c974435d921d327261bd33d64fbd47ec1
Kevin Shaffer <kshaffer@sentientscience.com>
2022-02-12 23:55:03 -0500

    making modal handle no previous games

--- src/components/modals/HistoryModal.tsx
+++ src/components/modals/HistoryModal.tsx
@@ -10,24 +10,35 @@ type Props = {
 }
 
 export const HistoryModal = ({ isOpen, previousGame, handleClose }: Props) => {
-  return (
-    <BaseModal title="Previous Game" isOpen={isOpen} handleClose={handleClose}>
-      <h2 className={`${previousGame.success ? 'text-green-600' : 'text-red-600'} my-4`}>
-        {previousGame.guesserName} 
-        {previousGame.success ? ` guessed ${previousGame.solution}!` : ` didn't guess ${previousGame.solution}...`}
-      </h2>
-      <Grid
-        resetGame={()=>{}}
-        allowedGuesses={previousGame.allowedGuesses!}
-        isCreatingSolution={false}
-        guesses={previousGame.guesses!}
-        currentGuess={''}
-        solution={previousGame.solution!}
-        isGuesser={false}
-        friendName={previousGame.guesserName!}
-        wordLength={previousGame.wordLength!}
-      ></Grid>
-      
-    </BaseModal>
-  )
+  if (previousGame && previousGame.solution){
+    return (
+      <BaseModal title="Previous Game" isOpen={isOpen} handleClose={handleClose}>
+        <h2 className={`${previousGame.success ? 'text-green-600' : 'text-red-600'} my-4`}>
+          {previousGame.guesserName} 
+          {previousGame.success ? ` guessed ${previousGame.solution}!` : ` didn't guess ${previousGame.solution}...`}
+        </h2>
+        <Grid
+          resetGame={()=>{}}
+          allowedGuesses={previousGame.allowedGuesses!}
+          isCreatingSolution={false}
+          guesses={previousGame.guesses!}
+          currentGuess={''}
+          solution={previousGame.solution!}
+          isGuesser={false}
+          friendName={previousGame.guesserName!}
+          wordLength={previousGame.wordLength!}
+        ></Grid>
+        
+      </BaseModal>
+    )
+  }
+  else {
+    return (
+      <BaseModal title="Previous Game" isOpen={isOpen} handleClose={handleClose}>
+        <h2 className="text-orange-400 my-4">
+          You have to play a game first!
+        </h2>        
+      </BaseModal>
+    )
+  }
 }
